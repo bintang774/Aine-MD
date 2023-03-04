8f82e608fe218738dd2dc6a5d117c6844f06339e
bintang774 <108184157+bintang774@users.noreply.github.com>
2023-03-04 21:08:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
+  ['62881038062602'],
   ['62895330379186'],
   ['62895330379186', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
